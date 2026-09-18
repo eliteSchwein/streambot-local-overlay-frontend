baf10dc5b27c99a7f2e1cc067331d43208e4197b
tludwig <business@tludwig.dev>
2026-09-18 20:40:04 +0200

    feat: add support for data-visible-name attribute

--- src/js/controller/VisibleController.ts
+++ src/js/controller/VisibleController.ts
@@ -4,33 +4,46 @@ import {Websocket} from "websocket-ts";
 export default class VisibleController extends BaseController {
     websocketEndpoints = ['notify_visible_element']
 
-    protected id = this.element.dataset.id
+    protected id: string
 
+    async postConnect() {
+        this.id =
+            this.element.getAttribute('data-visible-name')
+            ?? this.element.id
+    }
 
     async handleMessage(websocket: Websocket, method: string, data: any) {
-        if(!data) return
-        if(method === 'notify_test_mode') {
+        if (!data) return
+
+        if (method === 'notify_test_mode') {
             data.target = this.id
             data.state = data.active
         }
-        if(!data.target || data.target !== this.id) return
-        if(data.state === undefined) return
 
-        if(method !== 'notify_test_mode' && method !== 'notify_visible_element') return
+        if (!data.target || data.target !== this.id) return
+        if (data.state === undefined) return
+
+        if (
+            method !== 'notify_test_mode'
+            && method !== 'notify_visible_element'
+        ) return
 
         switch (data.state) {
             case true:
-                if(this.alertBoxHelper.isPresent()) {
+                if (this.alertBoxHelper.isPresent()) {
                     this.alertBoxHelper.show()
                     break
                 }
-                this.element.style.display = null
+
+                this.element.style.display = ''
                 break
+
             case false:
-                if(this.alertBoxHelper.isPresent()) {
+                if (this.alertBoxHelper.isPresent()) {
                     this.alertBoxHelper.hide()
                     break
                 }
+
                 this.element.style.display = 'none'
                 break
         }
